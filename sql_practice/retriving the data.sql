a8035275966f8eae797a0d6a9d931f66100c5889
Nidhisha08 <nidhishajain08@gmail.com>
2023-11-16 23:14:00 -0500

    updated the code

--- sql_practice/retriving the data.sql
+++ sql_practice/retriving the data.sql
@@ -1,3 +1,65 @@
+
+CREATE TABLE salesmen (
+    salesman_id INT PRIMARY KEY,
+    name VARCHAR(255),
+    city VARCHAR(255),
+    commission DECIMAL(5, 2)
+);
+
+INSERT INTO salesmen (salesman_id, name, city, commission)
+VALUES
+    (5001, 'James Hoog', 'New York', 0.15),
+    (5002, 'Nail Knite', 'Paris', 0.13),
+    (5005, 'Pit Alex', 'London', 0.11),
+    (5006, 'Mc Lyon', 'Paris', 0.14),
+    (5007, 'Paul Adam', 'Rome', 0.13),
+    (5003, 'Lauson Hen', 'San Jose', 0.12);
+
+
+CREATE TABLE orderss (
+    ord_no INT PRIMARY KEY,
+    purch_amt DECIMAL(8, 2),
+    ord_date DATE,
+    customer_id INT,
+    salesman_id INT
+);
+
+INSERT INTO orderss (ord_no, purch_amt, ord_date, customer_id, salesman_id)
+VALUES
+    (70001, 150.5, '2012-10-05', 3005, 5002),
+    (70009, 270.65, '2012-09-10', 3001, 5005),
+    (70002, 65.26, '2012-10-05', 3002, 5001),
+    (70004, 110.5, '2012-08-17', 3009, 5003),
+    (70007, 948.5, '2012-09-10', 3005, 5002),
+    (70005, 2400.6, '2012-07-27', 3007, 5001),
+    (70008, 5760, '2012-09-10', 3002, 5001),
+    (70010, 1983.43, '2012-10-10', 3004, 5006),
+    (70003, 2480.4, '2012-10-10', 3009, 5003),
+    (70012, 250.45, '2012-06-27', 3008, 5002),
+    (70011, 75.29, '2012-08-17', 3003, 5007),
+    (70013, 3045.6, '2012-04-25', 3002, 5001);
+
+
+create table grade(
+cust_id int primary key,
+cust_name varchar(20),
+city varchar(30),
+grade int ,
+sales_id int
+);
+
+INSERT INTO grade (cust_id, cust_name, city, grade, sales_id)
+VALUES
+    (3002, 'Nick Rimando', 'New York', 100, 5001),
+    (3007, 'Brad Davis', 'New York', 200, 5001),
+    (3005, 'Graham Zusi', 'California', 200, 5002),
+    (3008, 'Julian Green', 'London', 300, 5002),
+    (3004, 'Fabian Johnson', 'Paris', 300, 5006),
+    (3009, 'Geoff Cameron', 'Berlin', 100, 5003),
+    (3003, 'Jozy Altidor', 'Moscow', 200, 5007),
+    (3001, 'Brad Guzan', 'London', NULL, 5005);
+    
+
 select * from salesmen;
 #salespeople who come from the 'Paris' City or 'Rome' City. Return salesman_id, name, city, commission.
     
@@ -23,6 +85,20 @@ select * from salesmen where name like "N__L%" ;
 # whos name end with n
 select * from salesmen where name like "%n";
 
+CREATE TABLE testtable (
+    col1 VARCHAR(255) 
+);
+
+INSERT INTO testtable (col1)
+VALUES
+    ('A001/DJ-402\44_/100/2015'),
+    ('A001_\\DJ-402\\44_/100/2015'),
+    ('A001_DJ-402-2014-2015'),
+    ('A002_DJ-401-2014-2015'),
+    ('A001/DJ_401'),
+    ('A001/DJ_402\\44'),
+    ('A001/DJ_402\\44\\2015'),
+    ('A001/DJ-402%45\\2015/200');
 
 select * from testtable;
 # % : represent the charcter   and _ actually a single char if we want find the string contain _ char
